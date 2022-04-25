134fa668ad0952d3470dfc53652d8f429a59da89
karthikeyan <dkkarthik2000@gmail.com>
2022-04-25 18:11:16 +0530

    monday work

--- src/Components/ChatPage.jsx
+++ src/Components/ChatPage.jsx
@@ -2,7 +2,30 @@
 
 
 const ChatPage = () => {
-    return null
+    return (
+
+        <main className="chatsection">
+            <div className="chatbox">
+                <div className="contacts">
+                    <div className="client">
+                        <h1>Name</h1>
+                        <p>Last txt</p>
+                    </div>
+                    <div className="client">
+                        <h1>Name</h1>
+                        <p>Last txt</p>
+                    </div>
+                </div>
+                <div className="chatscreen">
+                    <div className="header"></div>
+                    <div className="chat"></div>
+                    <div className="chatinput"></div>
+                </div>
+
+            </div>
+        </main>
+
+    )
 }
 
 export default ChatPage
